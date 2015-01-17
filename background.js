6865e5e0bbfff76115ba19da8715f81eb5874918
Michael Zhang <failed.down@gmail.com>
2015-01-17 06:56:40 -0500

    crx pushes to firebase

--- background.js
+++ background.js
@@ -7,11 +7,12 @@ function bookmarkListener(id, changeInfo) {
 		uid: "some_id_we_will_figure_out_later",
 		url: changeInfo.url,
 		title: changeInfo.title,
+		numVotes: 0,
 	}, function(err) {
 		if (err) {
 			// shit
 		} else {
-			alert("yey updated");
+			// alert("yey updated");
 		}
 	});
 };
