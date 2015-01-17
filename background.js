fbfd9987d93aeeb48860ccfc211d6b4096f2ef21
gulci <bigmariobro@gmail.com>
2015-01-17 09:27:28 -0800

    Fixed and improved  JSON structure

--- background.js
+++ background.js
@@ -1,17 +1,19 @@
-var fb = new Firebase("https://bookmarks-nice.firebaseio.com/");
+var fb = new Firebase("https://bookmarks-nice.firebaseio.com");
 
 function bookmarkListener(id, changeInfo) {
-	// console.dir(changeInfo);
 	var msg = fb.push();
 	msg.set({
-		uid: "some_id_we_will_figure_out_later",
-		url: changeInfo.url,
-		title: changeInfo.title,
+                "post": {
+                        "uid": "some_id_we_will_figure_out_later",
+		        "url": changeInfo.url,
+		        "title": changeInfo.title,
+		        "numVotes": "0",
+                        }
 	}, function(err) {
 		if (err) {
-			// shit
+			// no.exe
 		} else {
-			alert("yey updated");
+			//alert("yey updated");
 		}
 	});
 };
